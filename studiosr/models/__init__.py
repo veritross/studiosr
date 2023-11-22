436a6a6983be6f93e18145dc29cccabeb256e281
sjjeong94 <sjjeong94@gmail.com>
2023-11-22 22:28:55 +0900

    set default values for maxsr arguments

--- studiosr/models/__init__.py
+++ studiosr/models/__init__.py
@@ -2,6 +2,7 @@ from studiosr.models.edsr import EDSR
 from studiosr.models.espcn import ESPCN
 from studiosr.models.han import HAN
 from studiosr.models.hat import HAT
+from studiosr.models.maxsr import MaxSR
 from studiosr.models.rcan import RCAN
 from studiosr.models.srcnn import SRCNN
 from studiosr.models.srresnet import SRResNet
